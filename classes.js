227bbeb4bfecfc0180e6da1c0b70eb76b9a7b901
lubanatest <32592478+lubanatest@users.noreply.github.com>
2017-10-07 17:31:00 +0400

    Update classes.js

--- classes.js
+++ classes.js
@@ -9,6 +9,12 @@ class Node
 	var from = "";
 	var to = "";
 	var latency = 0;
+
+	function Node(x,y,t){
+		this.from = x;
+		this.to = y;
+		this.latency = t;
+	}
 }
 // ----------------------------------
 // class Graph
@@ -16,14 +22,24 @@ class Node
 class Graph
 {
 	var connections = [];
-	const var count = 0;
+	var count = 0;
 
 	function Graph(){
+		this.count = 0;
+		this.connections = [];
 	}
 	
 	function addConnection(node){
 		this.connection[node.from].push(node);
 		this.count++;
+	}
+
+	function print(){
+		var data = "Graph";
+		for (var i=0;i<this.connection.length;i++){
+			data = data + "  " + this.connection[].from;
+		}
+		alert(data);
 	}
 	
 }
