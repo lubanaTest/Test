afada6a4919726447711caea8f3a15d9754e8ff3
lubanatest <32592478+lubanatest@users.noreply.github.com>
2017-10-08 03:55:39 +0400

    Update findPath.js

--- findPath.js
+++ findPath.js
@@ -39,6 +39,60 @@ function Graph(){
 // ----------------------------------
 
 
+
+
+
+function readCSV()
+{
+	
+	var graph = new Graph();
+	
+	var fileUpload = document.getElementById("csv");
+	var reader = new FileReader();
+	reader.readAsText(fileUpload.files[0]);
+	
+	reader.onload = function (e) {
+		var text = e.target.result;  
+		var rows = text.split("\n");
+		
+
+		for (var i = 0; i < rows.length; i++) {
+			
+			// For each line in the CSV File:
+			// insert data into Tree
+			// -------------------------------
+			
+			
+			var devices = rows[i].split(",");
+			var x = devices[0];
+			var y = devices[1];
+			var t = devices[2];
+			
+			if (x!=""){
+				//var node = new Node(x,y,t);
+				//graph.addConnection(node);
+				 
+			}
+			
+			
+			// -------------------------------
+			
+			
+		}
+		return graph;
+	}
+	
+	reader.onerror = function (e){
+		if(e.target.error.name == "NotReadableError") {
+			alert("Canno't read file !");
+		}
+		
+	}
+	
+	
+}
+
+
 // ------------------------------
 // Find path function:
 // recursive function to find the 
