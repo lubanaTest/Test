dc9381b731e8d607de40db7fe78a2f56a590fd59
lubanatest <32592478+lubanatest@users.noreply.github.com>
2017-10-08 03:54:26 +0400

    Update findPath.js

--- findPath.js
+++ findPath.js
@@ -1,3 +1,44 @@
+
+
+
+// ----------------------------------
+// class Node
+// ----------------------------------
+class Node
+{
+	
+	constructor(x,y,t){
+		this.from = x;
+		this.to = y;
+		this.latency = t;
+	}
+}
+// ----------------------------------
+// class Graph
+// ----------------------------------
+function Graph(){
+		this.count = 0;
+		this.connections = [];
+	}
+	
+	Graph.prototype.addConnection = function(node) {
+		this.connection[node.from].push(node);
+		this.count++;
+	};
+
+	Graph.prototype.print = function() {
+		var data = "Graph";
+		alert(this.connection.length);
+		for (var i=0;i<this.connection.length;i++){
+			//data = data + "  " + this.connection[i].from;
+		}
+		alert(data);
+	};
+	
+
+// ----------------------------------
+
+
 // ------------------------------
 // Find path function:
 // recursive function to find the 
