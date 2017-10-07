1c7cc69ca1df837005227a81e615a54271dbaeca
lubanatest <32592478+lubanatest@users.noreply.github.com>
2017-10-08 02:44:54 +0400

    Update classes.js

--- classes.js
+++ classes.js
@@ -28,8 +28,9 @@ function Graph(){
 
 	Graph.prototype.print = function() {
 		var data = "Graph";
+		alert(this.connection.length);
 		for (var i=0;i<this.connection.length;i++){
-			data = data + "  " + this.connection[i].from;
+			//data = data + "  " + this.connection[i].from;
 		}
 		alert(data);
 	};
