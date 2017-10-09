08494f904e90c85329bccee69828c8cb1c5006ea
lubanatest <32592478+lubanatest@users.noreply.github.com>
2017-10-09 04:05:43 +0400

    Update findPath.js

--- findPath.js
+++ findPath.js
@@ -135,6 +135,7 @@ function findPath(graph,X,Y,T)
 			 
 		for (var i=0;i<connections.length;i++){
 			var next = connections[i].to;
+			alert(next);
 			if (next == Y){
 				
 				path = path+X+" --> "+Y;
@@ -143,7 +144,8 @@ function findPath(graph,X,Y,T)
 			else
 			{
 				// call back
-				path = path + " \n " + findPath(graph,next,Y,T);
+				path = path+X+" --> "+next+ " \n ";
+				path = path  + findPath(graph,next,Y,T);
 				return path;
 				
 			}
@@ -157,5 +159,5 @@ function findPath(graph,X,Y,T)
 		}
 
 	}
-
+	return path;
 }
